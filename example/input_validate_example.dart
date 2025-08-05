3d71aa64adf895e8a33643dace773b3c6aba4c71
Firuz <vfiruz97@gmail.com>
2025-08-05 11:57:53 +0500

    init

---
+++ example/input_validate_example.dart
@@ -0,0 +1,6 @@
+import 'package:input_validate/input_validate.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/input_validate_test.dart
@@ -0,0 +1,16 @@
+import 'package:input_validate/input_validate.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
